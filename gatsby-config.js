adf5c9347a80d6d5acccd995a564af8bb096de86
0xMRTT <0xMRTT@tuta.io>
2022-09-03 12:05:10 +0200

    fix: format

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Atrophaneura',
     shortName: 'Atrophaneura project',
-    description: 'A WIP Desktop for Linux'
+    description: 'A WIP Desktop for Linux',
   },
   plugins: ['@primer/gatsby-theme-doctocat'],
 }
